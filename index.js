6ffc07382806977d1ecfd4ccb3f7982010144083
Dhiraj Kene <kdprakash1712@gmail.com>
2025-05-14 15:03:58 +0530

    cors remove

--- index.js
+++ index.js
@@ -12,7 +12,9 @@ const PORT = process.env.PORT || 8000;
 app.use(express.json());
 app.use("/user", UserRouter);
 app.use('/note', auth, NoteRouter)
-app.use(cors());
+// app.use(cors({
+//     origin:'*'
+// }))
 
 
 app.get('/', (req, res)=>{
