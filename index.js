a89b78fa317e46daef1bc34baf9c8b4d426e1013
Dhiraj Kene <kdprakash1712@gmail.com>
2025-05-15 12:32:43 +0530

    cors shifted

--- index.js
+++ index.js
@@ -9,10 +9,11 @@ const auth = require('./middleware/auth.middleware')
 
 const app = express();
 const PORT = process.env.PORT || 8000;
+app.use(cors());
 app.use(express.json());
 app.use("/user", UserRouter);
 app.use('/note', auth, NoteRouter)
-app.use(cors());
+
 
 
 app.get('/', (req, res)=>{
